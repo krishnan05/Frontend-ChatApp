46f9ddbf438d5d786941bed40364fbbf352b467e
krishnan05 <aayushkr@iitbhilai.ac.in>
2024-07-07 22:45:53 +0530

    Minor change in url

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://backend-chat-app-five.vercel.app/";
+export const host = "https://backend-chat-app-five.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const allUsersRoute = `${host}/api/auth/allUsers`;
